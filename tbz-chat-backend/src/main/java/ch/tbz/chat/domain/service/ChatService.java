9b7bf9fee8a19e135247a1bb3a8269a384897091
Severin <43531943+Tarkin25@users.noreply.github.com>
2020-09-25 08:52:35 +0200

    added application-dev.yaml

--- tbz-chat-backend/src/main/java/ch/tbz/chat/domain/service/ChatService.java
+++ tbz-chat-backend/src/main/java/ch/tbz/chat/domain/service/ChatService.java
@@ -3,8 +3,6 @@ package ch.tbz.chat.domain.service;
 import ch.tbz.chat.domain.model.Chat;
 import ch.tbz.chat.domain.model.User;
 
-import java.util.Collection;
-
 public interface ChatService {
 
     Chat create(Chat chat, User authenticated);

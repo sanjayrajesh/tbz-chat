6d6dc4398b2df948910e0be04b0ecd6715d82576
Severin <43531943+Tarkin25@users.noreply.github.com>
2020-09-30 22:24:24 +0200

    added OpenAPI backend documentation

--- tbz-chat-backend/src/main/java/ch/tbz/chat/config/SecurityConfiguration.java
+++ tbz-chat-backend/src/main/java/ch/tbz/chat/config/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package ch.tbz.chat.config;
 
 import ch.tbz.chat.domain.datatransfer.MappingStrategy;
 import ch.tbz.chat.domain.datatransfer.user.UserDTO;
-import ch.tbz.chat.domain.datatransfer.user.UserMappingStrategyFactory;
 import ch.tbz.chat.domain.model.User;
 import ch.tbz.chat.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,47 +23,67 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @DependsOn({"userMappingStrategy"})
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private final UserService userService;
-    private final MappingStrategy<UserDTO, User> userMappingStrategy;
-    private final JWTProperties jwtProperties;
+  private final UserService userService;
+  private final MappingStrategy<UserDTO, User> userMappingStrategy;
+  private final JWTProperties jwtProperties;
 
-    @Autowired
-    public SecurityConfiguration(UserService userService, MappingStrategy<UserDTO, User> userMappingStrategy, JWTProperties jwtProperties) {
-        this.userService = userService;
-        this.userMappingStrategy = userMappingStrategy;
-        this.jwtProperties = jwtProperties;
-    }
+  @Autowired
+  public SecurityConfiguration(
+      UserService userService,
+      MappingStrategy<UserDTO, User> userMappingStrategy,
+      JWTProperties jwtProperties) {
+    this.userService = userService;
+    this.userMappingStrategy = userMappingStrategy;
+    this.jwtProperties = jwtProperties;
+  }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
-    }
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+  }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .cors().and().csrf().disable()
-                .authorizeRequests().antMatchers(HttpMethod.POST, "/login", "/users", "/verification/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .addFilterAfter(loginHandler(), UsernamePasswordAuthenticationFilter.class)
-                .addFilterAfter(authenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.cors()
+        .and()
+        .csrf()
+        .disable()
+        .authorizeRequests()
+        .antMatchers(HttpMethod.POST, "/login", "/users", "/verification/**")
+        .permitAll()
+        .antMatchers(
+            "/swagger-ui.html",
+            "/v3/api-docs/**",
+            "/swagger-resources/**",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/api-docs.yaml")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .addFilterAfter(loginHandler(), UsernamePasswordAuthenticationFilter.class)
+        .addFilterAfter(authenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+  }
 
-    @Bean
-    public static BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  public static BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-    @Bean
-    public LoginHandler loginHandler() throws Exception {
-        return new LoginHandler(new AntPathRequestMatcher("/login", "POST"), authenticationManager(), userMappingStrategy, jwtProperties);
-    }
+  @Bean
+  public LoginHandler loginHandler() throws Exception {
+    return new LoginHandler(
+        new AntPathRequestMatcher("/login", "POST"),
+        authenticationManager(),
+        userMappingStrategy,
+        jwtProperties);
+  }
 
-    @Bean
-    public AuthenticationFilter authenticationFilter() {
-        return new AuthenticationFilter(userService, jwtProperties);
-    }
-
+  @Bean
+  public AuthenticationFilter authenticationFilter() {
+    return new AuthenticationFilter(userService, jwtProperties);
+  }
 }
